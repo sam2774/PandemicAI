fc39bd1483077bd86267c958f1107bc71cf7aa75
Thomas Simeroth <thomassimeroth@gmail.com>
2017-11-21 16:04:16 -0600

    Added yellow edges
    Halfway there

--- pandemic.py
+++ pandemic.py
@@ -18,3 +18,17 @@ pandemic.add_edges_from([("Essen", "London"), ("Essen", "Paris"), ("Essen", "Mil
 pandemic.add_edges_from([("Paris", "London"), ("Paris", "Madrid"), ("Paris", "Algiers"), ("Paris", "Milan"), ("Paris", "Essen")])
 pandemic.add_edges_from([("Milan", "Essen"), ("Milan", "Paris"), ("Milan", "Instanbul")])
 pandemic.add_edges_from([("St. Petersburg", "Essen"), ("St. Petersburg", "Istanbul"), ("St. Petersburg", "Moscow")])
+
+pandemic.add_edges_from([("Los Angeles", "Sydney"), ("Los Angeles", "San Fransisco"), ("Los Angeles", "Chicago"), ("Los Angeles", "Mexico City")])
+pandemic.add_edges_from([("Mexico City", "Los Angeles"), ("Mexico City", "Chicago"), ("Mexico City", "Miami"), ("Mexico City", "Bogota"), ("Mexico City", "Lima")])
+pandemic.add_edges_from([("Miami", "Atlanta"), ("Miami", "Washington"), ("Miami", "Bogota"), ("Miami", "Mexico City")])
+pandemic.add_edges_from([("Bogota", "Mexico City"), ("Bogota", "Miami"), ("Bogota", "Sao Paolo"), ("Bogota", "Buenos Aires"), ("Bogota", "Lima")])
+pandemic.add_edges_from([("Lima", "Mexico City"), ("Lima", "Bogota"), ("Lima", "Santiago")])
+pandemic.add_edges_from([("Santiago", "Lima"), ("Santiago", "Buenos Aires")])
+pandemic.add_edges_from([("Buenos Aires", "Santiago"), ("Buenos Aires", "Bogota"), ("Buenos Aires", "Sao Paulo")])
+pandemic.add_edges_from([("Sao Paulo", "Bogota"), ("Sao Paulo", "Buenos Aires"), ("Sao Paulo", "Madrid"), ("Sao Paulo", "Lagos")])
+pandemic.add_edges_from([("Lagos", "Sao Paulo"), ("Lagos", "Kinshasa"), ("Lagos", "Khartoum")])
+pandemic.add_edges_from([("Kinshasa", "Lagos"), ("Kinshasa", "Johanesburg"), ("Kinshasa", "Khartoum")])
+pandemic.add_edges_from([("Johanesburg", "Kinshasa"), ("Johanesburg", "Khartoum")])
+pandemic.add_edges_from([("Khartoum", "Lagos"), ("Khartoum", "Kinshasa"), ("Khartoum", "Johanesburg"), ("Khartoum", "Cairo")])
+
